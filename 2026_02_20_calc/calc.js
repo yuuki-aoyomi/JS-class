d44ea1a18ff81a1d8e2ba5d28c13bcadfe7bb077
aoyomi <hikoukiukiuki@vesta.ocn.ne.jp>
2026-02-20 16:09:23 +0900

    add calc system

---
+++ 2026_02_20_calc/calc.js
@@ -0,0 +1,9 @@
+exports.num = 1;
+
+exports.add = (a, b) => {
+    return a + b;
+};
+
+exports.sub = (a, b) => {
+    return a - b;
+};
---
+++ 2026_02_20_calc/index.js
@@ -0,0 +1,9 @@
+const calc = require("./calc");
+
+console.log(calc.num);
+
+let res = calc.add(3, 1);
+console.log(res);
+
+res = calc.sub(3, 1);
+console.log(res);
